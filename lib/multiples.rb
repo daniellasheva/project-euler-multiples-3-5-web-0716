fee3ab7a9a5e1849a37094aace7431439d32ab84
daniellasheva <daniellasheva@gmail.com>
2016-11-21 16:30:59 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
 # Enter your procedural solution here!
+
+#find all the numbers below 1000 that are multiples of
+#3 or 5
+
+
+def collect_multiples(limit)
+  x=1
+  array=[]
+  while (x<limit)
+    if x%3 == 0 || x%5 == 0
+      array.push(x)
+    end
+    x+=1
+  end
+  array
+end
+
+
+
+def sum_multiples(limit)
+  array= collect_multiples(limit)
+  total= array.reduce(:+)
+  total
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,31 @@
 # Enter your object-oriented solution here!
+require 'pry'
+
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit=limit
+  end
+
+  def collect_multiples
+    x=1
+    array=[]
+    while (x<@limit)
+      if x%3 == 0 || x%5 == 0
+        array.push(x)
+      end
+      x+=1
+    end
+    array
+  end
+
+  def sum_multiples
+    array= collect_multiples
+    total= array.reduce(:+)
+    total
+  end
+
+
+end
